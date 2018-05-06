9b2fff8abb4dad4181146b64539b4a1df98ef6d2
FirstKlaas <klaas.nebuhr@gmail.com>
2018-05-06 22:08:51 +0200

    Log Message

--- fake_plant/index.js
+++ fake_plant/index.js
@@ -6,3 +6,5 @@ let mqttConfig = config.get('MqttServer');
 
 let mqtt_ip = mqttConfig.get('ip');
 let mqtt_port = mqttConfig.get('port')
+
+console.log(`Using MqttServer at ${mqtt_ip}:${mqtt_port}`);
