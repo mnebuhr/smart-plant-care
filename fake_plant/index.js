1dd301e0456dd788758156e92ceeff9cf2790b03
FirstKlaas <klaas.nebuhr@gmail.com>
2018-05-07 07:00:26 +0200

    Doing something periodically using the configured value.

--- fake_plant/index.js
+++ fake_plant/index.js
@@ -3,8 +3,16 @@
 let config = require('config');
 
 let mqttConfig = config.get('MqttServer');
+let plantsConfig = config.get('Plants');
 
 let mqtt_ip = mqttConfig.get('ip');
 let mqtt_port = mqttConfig.get('port')
 
+let intervall = plantsConfig.get('intervall') * 1000;
+
 console.log(`Using MqttServer at ${mqtt_ip}:${mqtt_port}`);
+
+let interValObj = setInterval(() => {
+	let randVal = Math.floor(Math.random() * Math.floor(5));
+	console.log(`Sending plant data ${randVal}`);
+}, intervall)
