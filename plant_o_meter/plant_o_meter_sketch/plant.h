9ebda49e5087e8d5ec6ce892f81297fe4cf249f7
FirstKlaas <klaas.nebuhr@gmail.com>
2018-05-07 08:07:37 +0200

    Initial commit (Not working)

---
+++ plant_o_meter/plant_o_meter_sketch/plant.h
@@ -0,0 +1,9 @@
+#ifndef PLANT_H
+#define PLANT_H
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+#include <arduino.h>
+
+#endif
---
+++ plant_o_meter/plant_o_meter_sketch/plant_o_meter_sketch.ino
@@ -0,0 +1,22 @@
+/***************************************************************
+ * plant-o-meter
+ * 
+ * Part of the smart-plant-care project.
+ * 
+ * Author  : Marion Nebuhr, Klaas Nebuhr
+ * Created : 07.05.2018
+ */
+
+ #include "plant.h"
+
+const char* ssid     = "xxx";      
+const char* password = "YYY";
+const char* mqtt_server = "xxx.xxx.xxx.xxx";
+
+void setup() {
+
+}
+
+void loop() {
+
+}
